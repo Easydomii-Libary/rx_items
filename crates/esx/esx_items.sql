a8d073061519910c971b97a1299eb3b456f5cc90
Domi <holenstein.dominic@gmail.com>
2024-05-27 19:53:55 +0200

    Update esx_items.sql

--- crates/esx/esx_items.sql
+++ crates/esx/esx_items.sql
@@ -1,21 +1,21 @@
-DELETE FROM items WHERE name = 'bone_fragments';
-DELETE FROM items WHERE name = 'cloth';
-DELETE FROM items WHERE name = 'leather';
-DELETE FROM items WHERE name = 'raw_pork';
-DELETE FROM items WHERE name = 'animal_fat';
-DELETE FROM items WHERE name = 'meat_stand';
-DELETE FROM items WHERE name = 'deer';
-DELETE FROM items WHERE name = 'boar_meat';
-DELETE FROM items WHERE name = 'deer_horn';
+DELETE FROM items WHERE name = 'redkeycard';
+DELETE FROM items WHERE name = 'greenkeycard';
+DELETE FROM items WHERE name = 'bluekeycard';
+DELETE FROM items WHERE name = 'codelock';
+DELETE FROM items WHERE name = 'boxtreebig';
+DELETE FROM items WHERE name = 'boxtwobig';
+DELETE FROM items WHERE name = 'boxtwosmall';
+DELETE FROM items WHERE name = 'boxonesmall';
+DELETE FROM items WHERE name = 'boxonebig';
 
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES
-	('bone_fragments', 'Bone Fragments', 1),
-	('cloth', 'Cloth', 1),
-	('leather', 'Leather', 1),
-	('raw_pork', 'Raw Pork', 1),
-	('animal_fat', 'Animal Fat', 1),
-	('meat_stand', 'Meat Bait', 1),
-	('deer', 'Deer Bait', 1),
-	('boar_meat', 'Boar Bait', 1),
-	('deer_horn', 'Deer Horn', 1)
+	('redkeycard', 'Red Keycard', 1),
+	('greenkeycard', 'Green Keycard', 1),
+	('bluekeycard', 'Blue Keycard', 1),
+	('codelock', 'Codelock', 1),
+	('boxtreebig', 'Wooden Storage Big', 1),
+	('boxtwobig', 'Wooden Storage Big', 1),
+	('boxtwosmall', 'Wooden Storage Small', 1),
+	('boxonesmall', 'Wooden Storage Small', 1),
+	('boxonebig', 'Wooden Storage Big', 1)
 ;
